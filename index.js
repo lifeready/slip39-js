92882153bc70135c98ce0b8385a48c387bd0d40a
David Fernandez <i.am.david.fernandez@gmail.com>
2020-06-26 20:52:46 +1000

    Updated module exports.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require('./src/slip39');
+import * as Slip39 from './src/slip39';
+import * as Slip39Helper from './src/slip39_helper';
+
+module.exports = {
+  Slip39,
+  Slip39Helper,
+};
